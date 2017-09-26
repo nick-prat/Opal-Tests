4be0b2a10431116dfd8fa877570dc3088932bad5
Nick Prat <npissoawsome@gmail.com>
2017-09-26 04:14:05 -0400

    Added proper input controls and test model

--- test/scene.cc
+++ test/scene.cc
@@ -1,5 +1,7 @@
 #include "scene.hh"
 
+#include <Opal/Components/components.hh>
+
 using namespace Opal;
 
 TestScene::TestScene(Display& display, const std::string& name)
@@ -7,21 +9,35 @@ TestScene::TestScene(Display& display, const std::string& name)
     m_display.setCursorCapture(true);
     m_display.setCursorPosition({0.0, 0.0});
 
-    m_display.bindCursorUpdate([&display = m_display](int xpos, int ypos) {
+    m_display.bindCursorUpdate([&display = m_display](const float xpos, const float ypos) {
         display.getCamera().rotateCamera({ypos / 500.0f, xpos / 500.0f, 0.0f});
         display.setCursorPosition({0.0f, 0.0f});
     });
 
-    m_display.bindWhileKeyPressed(InputKey::D, [&camera = m_display.getCamera()](InputKey key) {
-        auto dir = camera.getDirection();
-        camera.moveCamera({-dir.z * 0.1f, 0.0f, dir.x * 0.1f});
+    m_display.bindWhileKeyPressed(InputKey::W, [&camera = m_display.getCamera()] (const InputKey key) {
+        camera.moveCamera(camera.getDirection());
     });
 
-    m_display.bindWhileKeyPressed(InputKey::A, [&camera = m_display.getCamera()](InputKey key) {
-        auto dir = camera.getDirection();
-        camera.moveCamera({dir.z * 0.1f, 0.0f, -dir.x * 0.1f});
+    m_display.bindWhileKeyPressed(InputKey::S, [&camera = m_display.getCamera()] (const InputKey key) {
+        camera.moveCamera(camera.getDirection() * -1.0f);
+    });
+
+    m_display.bindWhileKeyPressed(InputKey::D, [&camera = m_display.getCamera()] (const InputKey key) {
+        camera.moveCamera(glm::cross(camera.getDirection(), camera.getUpDirection()));
+    });
+
+    m_display.bindWhileKeyPressed(InputKey::A, [&camera = m_display.getCamera()] (const InputKey key) {
+        camera.moveCamera(glm::cross(camera.getUpDirection(), camera.getDirection()));
     });
 
     m_display.setWireFrame(true);
-    m_entityManager.registerSystem<NormalRenderSystem>(m_assetStore.getShader(NormalRenderSystem::shaderName), m_display, m_worldLight);
+
+    const auto& textures = m_assetStore.getTextures();
+    for(const auto& [name, tex] : textures) {
+        std::cout << name << '\n';
+    }
+
+    auto id = m_entityManager.createEntity();
+    m_entityManager.createComponent<CRender>(id, m_assetStore.getModel3D("deer"));
+    m_entityManager.createComponent<CBody>(id, glm::vec3{0.0f, 0.0f, 0.0f});
 }
